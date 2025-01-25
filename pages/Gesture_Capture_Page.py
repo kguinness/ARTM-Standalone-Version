d783ee48a46cedfe8396bb546f6bad56d3f8f13d
Kristina Riesco <riescokristina@gmail.com>
2025-01-24 23:15:05 -0500

    version 2.1.4

--- pages/Gesture_Capture_Page.py
+++ pages/Gesture_Capture_Page.py
@@ -3,7 +3,9 @@ import sys
 import time
 import numpy as np
 import mediapipe as mp
+import requests
 import webbrowser
+
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QApplication
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer
@@ -23,12 +25,21 @@ class GestureCapturePage(QWidget):
 
         # Set up the page layout
         self.layout = QVBoxLayout()
+        #self.layout.addWidget(self.video_label)
+        #self.video_label("Gesture detection in progress...")
         self.setLayout(self.layout)
         self.google_opened = False  # Flag to track if Google has been opened
+
+
+        # Qlabel for gesture detection status
+        self.status_label = QLabel("Gesture detection in progress...")
+        self.layout.addWidget(self.status_label)
+
 
         # Create a label to display the video feed
         self.video_label = QLabel()
         self.layout.addWidget(self.video_label)
+
 
         # Initialize video capture
         self.cap = cv2.VideoCapture(0)
@@ -55,7 +66,7 @@ class GestureCapturePage(QWidget):
             'L Sign': 0
         }
 
-        self.GESTURE_DETECTION_TIME = 1.5  # 1.5 seconds
+        self.GESTURE_DETECTION_TIME = 1.0  # 1 second
 
     # Insert Gesture Box in Bottom Right Corner
     def draw_box_on_frame(self, frame):
@@ -71,10 +82,6 @@ class GestureCapturePage(QWidget):
 
         # Draw the rectangle
         cv2.rectangle(frame, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (250, 0, 0), 2)
-
-
-
-
 
 
     def update_frame(self):
@@ -111,9 +118,11 @@ class GestureCapturePage(QWidget):
                 # Check for gestures
 
                 if detect_peace_sign(hand_landmarks):
+                    gesture = 'Peace Sign Detected'
                     if self.gesture_timers['Peace Sign'] == 0:
                         self.gesture_timers['Peace Sign'] = time.time()
-                    elif time.time() - self.gesture_timers['Peace Sign'] >= self.GESTURE_DETECTION_TIME:
+                    #elif time.time() - self.gesture_timers['Peace Sign'] >= self.GESTURE_DETECTION_TIME:
+                    elif time.time() - self.gesture_timers['Peace Sign'] >= 3: #3 seconds
                         gesture = 'Peace Sign Detected'
                         if not self.google_opened:  # Only open Google once
                             webbrowser.open("https://www.google.com")
@@ -126,50 +135,56 @@ class GestureCapturePage(QWidget):
 
 
                 if detect_thumbs_up(hand_landmarks):
+                    gesture = 'Thumbs Up Detected'
                     if self.gesture_timers['Thumbs Up'] == 0:
                         self.gesture_timers['Thumbs Up'] = time.time()
-                    elif time.time() - self.gesture_timers['Thumbs Up'] >= self.GESTURE_DETECTION_TIME:
+                    elif time.time() - self.gesture_timers['Thumbs Up'] >= 3:
                         gesture = 'Thumbs Up Detected'
                 else:
                     self.gesture_timers['Thumbs Up'] = 0
 
                 if detect_index_upwards(hand_landmarks):
+                    gesture = 'Index Up Detected'
                     if self.gesture_timers['Index Up'] == 0:
                         self.gesture_timers['Index Up'] = time.time()
-                    elif time.time() - self.gesture_timers['Index Up'] >= self.GESTURE_DETECTION_TIME:
+                    elif time.time() - self.gesture_timers['Index Up'] >= 3:
                         gesture = 'Index Up Detected'
                 else:
                     self.gesture_timers['Index Up'] = 0
 
                 if detect_rock_and_roll_salute(hand_landmarks):
+                    gesture = 'Rock and Roll Salute Detected'
                     if self.gesture_timers['Rock and Roll Salute'] == 0:
                         self.gesture_timers['Rock and Roll Salute'] = time.time()
-                    elif time.time() - self.gesture_timers['Rock and Roll Salute'] >= self.GESTURE_DETECTION_TIME:
+                    elif time.time() - self.gesture_timers['Rock and Roll Salute'] >= 3:
                         gesture = 'Rock and Roll Salute Detected'
                 else:
                     self.gesture_timers['Rock and Roll Salute'] = 0
 
                 if detect_fist(hand_landmarks):
+                    gesture = 'Fist Gesture Detected'
                     if self.gesture_timers['Fist'] == 0:
                         self.gesture_timers['Fist'] = time.time()
-                    elif time.time() - self.gesture_timers['Fist'] >= self.GESTURE_DETECTION_TIME:
+                    elif time.time() - self.gesture_timers['Fist'] >= 3:
                         gesture = 'Fist Gesture Detected'
                 else:
                     self.gesture_timers['Fist'] = 0
 
                 if detect_letter_l(hand_landmarks):
+                    gesture = 'L Sign Detected'
                     if self.gesture_timers['L Sign'] == 0:
                         self.gesture_timers['L Sign'] = time.time()
-                    elif time.time() - self.gesture_timers['L Sign'] >= self.GESTURE_DETECTION_TIME:
+                    elif time.time() - self.gesture_timers['L Sign'] >= 3:
                         gesture = 'L Sign Detected'
                 else:
                     self.gesture_timers['L Sign'] = 0
+
 
         # Draw the gesture box in the bottom-right corner
         self.draw_box_on_frame(frame)
 
         # Display the gesture on the frame
-        cv2.putText(frame, gesture, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        #cv2.putText(frame, gesture, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         # Convert the frame back to QImage to display in QLabel
         height, width, channel = frame.shape
@@ -182,9 +197,17 @@ class GestureCapturePage(QWidget):
         q_img = QImage(frame_rgb.data, width, height, bytes_per_line, QImage.Format_RGB888)
 
 
-
         # Set the pixmap of the video_label to the new frame
         self.video_label.setPixmap(QPixmap.fromImage(q_img))
+
+        # Update the status lebel with the detected gesture
+        if gesture != 'No gesture detected':
+            self.status_label.setText(f"Detected Gesture: {gesture}")
+        else:
+            self.status_label.setText("Gesture detection in progress...")
+
+
+
 
     def closeEvent(self, event):
         # Release the video capture when closing the page
